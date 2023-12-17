90ef3703a1ebbe2e8f8100e4b79ebf9617ad701e
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-17 19:19:56 +0900

    ResetButton 추가

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import './Counter.css'
 import {useState} from "react";
-import CounterButton from './CounterButton';
+import CounterButton from './CounterButton.jsx';
 
 export default function Counter(){
 
@@ -10,12 +10,17 @@ export default function Counter(){
         setCount(count + by)
     }
 
+    function resetCounter(){
+        setCount(0);
+    }
+
     return (
         <>
             <span className="totalCount">{count}</span>
             <CounterButton by={1} incrementMethod={incrementCounterFunction}/>
             <CounterButton by={2} incrementMethod={incrementCounterFunction}/>
             <CounterButton by={3} incrementMethod={incrementCounterFunction}/>
+            <button className="resetButton" onClick={resetCounter}>Reset</button>
         </>
     
     )
