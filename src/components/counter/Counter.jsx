1b811e99d5ea67b53e173d55b0913adc6db0ac0c
Hyeyeon-Ko <khy33355@gmail.com>
2023-12-17 19:01:50 +0900

    Counter 컴포넌트 property 설정

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,22 +1,34 @@
 import './Counter.css'
 import {useState} from "react";
+import PropTypes from "prop-types";
 
-export default function Counter(){
+export default function Counter({by}){
 
     const [count, setCount] = useState(0);
 
     function incrementCounterFunction(){
-        setCount(count + 1)
+        setCount(count + by)
+    }
+
+    function decrementCounterFunction(){
+        setCount(count - by)
     }
 
     return(
         <div className="Counter">
             <span className="count">{count}</span>
             <div>
-                <button className="counterButton" 
-                        onClick={incrementCounterFunction}>+1
-                </button>
+                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
+                <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
             </div>
         </div>
     )
+}
+
+Counter.propTypes = {
+    by: PropTypes.number
+}
+
+Counter.defaultProps = {
+    by: 1
 }
